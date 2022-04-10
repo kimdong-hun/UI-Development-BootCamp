a86261c07a219df264114dcec3e35ad3dae7e7a0
kimdong-hun <kimdonghun426@gmail.com>
2022-04-11 00:57:48 +0900

    🚧 Implement deleting all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,24 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+
+function closeGnbSearchHistory(e) {
+  if (!gnbSearch.contains(e.target)) {
+    gnbSearchHistory.classList.remove('is-active')
+    window.removeEventListener('click', closeGnbSearchHistory)
+  }
+}
 
 function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length == 0) {
+    return
+  }
+
   if (!gnbSearch.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
@@ -12,9 +28,9 @@ function openGnbSearchHistory() {
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 
-function closeGnbSearchHistory(e) {
-  if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistory)
-  }
+function deleteAllSearchHistoryItems() {
+  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
 }
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
