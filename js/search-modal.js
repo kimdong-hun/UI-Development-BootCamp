46d825b1c45d0d5f3dcee6751033017bbb0bb060
kimdong-hun <kimdonghun426@gmail.com>
2022-04-13 22:35:32 +0900

    🚧 Implement deleting all search-history-items and deleting individual search-history-item

--- js/search-modal.js
+++ js/search-modal.js
@@ -1,20 +1,44 @@
 const searchModal = document.querySelector('.search-modal')
-const searchOverlay = document.querySelector('.overlay')
 const searchButton = document.querySelector('.gnb-icon-button.is-search')
 const searchModalCloseButton = searchModal.querySelector(
   '.search-modal-form .btn-ghost'
 )
 
+const searchModalHistory = searchModal.querySelector('.search-history')
+const searchModalHistoryList = searchModalHistory.querySelector('ol')
+
+const searchModalDeleteAllButton = searchModalHistory.querySelector(
+  '.search-history-header button'
+)
+const searchModalDeleteButtonList =
+  searchModalHistoryList.querySelectorAll('.delete-button')
+
 function openSearchModal() {
   searchModal.classList.add('is-active')
-  searchOverlay.classList.add('is-active')
 }
 
 searchButton.addEventListener('click', openSearchModal)
 
 function closeSearchModal() {
   searchModal.classList.remove('is-active')
-  searchOverlay.classList.remove('is-active')
 }
 
 searchModalCloseButton.addEventListener('click', closeSearchModal)
+
+function deleteAllSearchModalHistoryItems() {
+  searchModalHistoryList.innerHTML = ''
+}
+
+searchModalDeleteAllButton.addEventListener(
+  'click',
+  deleteAllSearchModalHistoryItems
+)
+
+function deleteSearchModalHistoryItem() {
+  const modalItemToDelete = this.parentNode
+  searchModalHistoryList.removeChild(modalItemToDelete)
+}
+
+searchModalDeleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchModalHistoryItem)
+})
